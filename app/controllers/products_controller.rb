0410f1a3ba1eabf9a13e721fc9dabeeed425cde9
p1p13 <panand@grepruby.com>
2017-09-21 12:02:43 +0530

    added facility for uploading images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -55,6 +55,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:title, :description, :in_stock, :cost)
+    params.require(:product).permit(:title, :description, :in_stock, :cost, :image)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_attached_file :image
+  validates_attachment_content_type :image  , content_type: /\Aimage\/.*\z/
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
   validates_presence_of :title, :description, :in_stock, :cost
