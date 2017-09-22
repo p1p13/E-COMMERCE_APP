a6059a076fc1a47b17cd133811ba67e6fe234107
p1p13 <panand@grepruby.com>
2017-09-22 18:30:50 +0530

    modified orders controller's create action so that all order items are saved along with order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,35 +5,33 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = current_user.orders.new(order_params) 
-    @cart = current_user.cart 
-    net_cost = 0
-    begin
-      Order.transaction do
-        if @order.save!      
-          count = 0
-          @cart.cart_items.each do |cart_item|
-            count += 1
-            net_cost += cart_item.cost 
-            cart_item.destroy!
-            product = cart_item.product
-            stock_left = product.in_stock - cart_item.quantity
-            product.update_attributes!(in_stock: stock_left)
-            order_item_params = { product_id: cart_item.product_id, quantity: cart_item.quantity }
-            @order_item = @order.order_items.create!(order_item_params)
-          end
-          if count == 0
-            raise 'your cart is empty'
-          end
-          @order.update_attributes!(transaction_id: SecureRandom.hex(10), net_cost: net_cost, status: "Order Recieved")
-          redirect_to order_path(@order)
-          OrderMailer.order_email(@order).deliver_later
-        end 
+    cart = current_user.cart 
+    if cart.cart_items.empty?
+      flash[:danger] = "your cart is empty"
+      redirect_to your_cart_cart_index_path
+    else
+      @order = current_user.orders.new(order_params) 
+      net_cost = 0
+      cart.cart_items.each do |cart_item|
+        net_cost += cart_item.cost 
+        order_item_params = { product_id: cart_item.product_id, quantity: cart_item.quantity }
+        @order.order_items.build(order_item_params)
       end
-    rescue Exception => e
-      flash.now[:danger] = e.message
-      render 'new'
-    end   
+      if @order.save
+        @order.update_attributes(transaction_id: SecureRandom.hex(10), net_cost: net_cost, status: "Order Recieved")
+        cart.cart_items.each do |cart_item| 
+          cart_item.destroy
+          product = cart_item.product
+          stock_left = product.in_stock - cart_item.quantity
+          product.update_attributes(in_stock: stock_left)
+        end
+        redirect_to order_path(@order)
+        OrderMailer.order_email(@order).deliver_later
+      else
+        flash.now[:danger] = @order.errors.full_messages.join(', ')
+        render 'new'
+      end 
+    end
   end
 
   def show
