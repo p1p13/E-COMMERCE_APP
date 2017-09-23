8766ae9c394768b0337d146b25424ff194911138
Piyush Anand <piyushanand211@yahoo.com>
2017-09-23 10:45:54 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,9 +56,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
      address:              'smtp.gmail.com',
      port:                 587,
-     domain:               'gmail.com',
-     user_name:            '80511qw@gmail.com',
-     password:             '8051qwert',
+     domain:               'gmail.com'
      authentication:       'plain',
      enable_starttls_auto: true  
   }
